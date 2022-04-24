df96ac6c71fc3eec73264452c6ab3660411e9f19
Hubert Osypowicz <31357151+PelikanFix16@users.noreply.github.com>
2022-04-24 22:30:40 +0200

    Creating installation for application

---
+++ TaskManager/Application.Core/ApplicationInstallation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Service.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public static class ApplicationInstallation
+    {
+        public static IServiceCollection AddTaskApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<ITaskService,TaskService>();
+            return services;
+
+        }
+    }
+}
