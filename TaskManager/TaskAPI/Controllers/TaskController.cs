9442e5ac201aadd4210789cc66e531fdf6848630
Hubert Osypowicz <31357151+PelikanFix16@users.noreply.github.com>
2022-04-24 23:02:44 +0200

    Simple start point for controller TaskAPI

---
+++ TaskManager/TaskAPI/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Core;
+using Service.Core.DTO;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        private readonly ITaskService _service;
+
+        public TaskController(ITaskService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<TaskModelOutput>> GetAllTasks()
+        {
+            var taks = _service.GetAllTask().ToList();
+            return Ok(taks);
+        }
+
+    }
+}
