50598af0ce98611fea484504a6d045fb2e0178b0
Hubert Osypowicz <31357151+PelikanFix16@users.noreply.github.com>
2022-04-24 19:25:59 +0200

    Domain Core
    Class library for core in domain. This project will store entities models also can store value objects but for this example i do not create value objects.

---
+++ TaskManager/Domain.Core/TaskModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Core
+{
+    public class TaskModel
+    {
+        public Guid Id { get; private set; }
+        public DateTimeOffset CreatedDate { get; private set; }
+        public DateTimeOffset? ExpirationDate { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public int CompleteStatus { get; private set; }
+
+        public TaskModel(DateTimeOffset? expirationDate,string title,string description)
+        {
+            CreatedDate = DateTimeOffset.Now;
+            ExpirationDate = expirationDate;
+            Title = title;
+            Description = description;
+            CompleteStatus = 0;
+            Id = Guid.NewGuid();
+        }
+
+    }
+}
