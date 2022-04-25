35255c4a4f91093e8923c64d6014b68697e6ce1e
Hubert Osypowicz <31357151+PelikanFix16@users.noreply.github.com>
2022-04-25 23:54:58 +0200

    Delete test persistence ef

--- TaskManager/Domain.Core/TaskModel.cs
+++ TaskManager/Domain.Core/TaskModel.cs
@@ -9,16 +9,21 @@ namespace Domain.Core
     public class TaskModel
     {
         public Guid Id { get; private set; }
-        public DateTimeOffset CreatedDate { get; private set; }
+        public DateTimeOffset? CreatedDate { get; private set; }
         public DateTimeOffset? ExpirationDate { get; private set; }
         public string Title { get; private set; }
         public string? Description { get; private set; }
         public int CompleteStatus { get; private set; }
 
-        public TaskModel(DateTimeOffset? expirationDate,string title,string? description)
+        public TaskModel(DateTimeOffset? expirationDate,DateTimeOffset? createdDate, string title,string? description)
         {
-            CreatedDate = DateTimeOffset.Now;
-            UpdateExpirationDate(expirationDate);
+            if (createdDate == null)
+                CreatedDate = DateTimeOffset.Now;
+            else
+                CreatedDate = createdDate.GetValueOrDefault();
+                
+            if(expirationDate != null)
+                UpdateExpirationDate(expirationDate);
             Title = title;
             Description = description;
             CompleteStatus = 0;
