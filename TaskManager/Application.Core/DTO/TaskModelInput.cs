0fd06802fab09e03007d57416eb14762827a32aa
Hubert Osypowicz <31357151+PelikanFix16@users.noreply.github.com>
2022-04-26 01:13:21 +0200

    Dummy mapping

--- TaskManager/Application.Core/DTO/TaskModelInput.cs
+++ TaskManager/Application.Core/DTO/TaskModelInput.cs
@@ -12,6 +12,7 @@ namespace Service.Core.DTO
         public string? Description { get; set; }
         public DateTimeOffset? ExpirationDate { get; set; }
 
+        public DateTimeOffset? CreatedDate;
 
     }
 }
